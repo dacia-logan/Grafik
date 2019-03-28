8544b1a9f82ecf1720b492b30341547e066771c1
dacia-logan <grimmeeh@gmail.com>
2019-03-28 16:49:11 +0000

    eitt rotate komið, eftir að implementa fleiri en eitt í einu

--- cube.js
+++ cube.js
@@ -7,14 +7,18 @@ class Cube {
         this.x=x;
         this.y=y;
         this.z=z;
-        this.rot=0;
+
+
+        this.rotX=0;
+        this.rotY=0;
+        this.rotZ=0;
     }
 
     draw(mv){
         gl.bindBuffer( gl.ARRAY_BUFFER, blockBuffer );
         gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
 
-        this.rot+=2;
+        
         var mvTemp = mv;
 
         gl.uniform4fv( colorLoc, vec4(1.0, 0, 0.0, 1.0) );
@@ -22,7 +26,6 @@ class Cube {
 
         var mv2 = mat4();
         mv2 = mult( mvTemp, translate(this.x, this.y, this.z));
-        mv2=mult(mv2,rotateZ(this.rot))
         gl.uniformMatrix4fv(matrixLoc, false, flatten(mv2));
         gl.drawArrays( gl.TRIANGLES, 0,  NumVertices);
 
@@ -30,8 +33,11 @@ class Cube {
 
 
         var mv1 = mat4();
+
         mv1 = mult( mvTemp, translate(this.x, this.y+1, this.z));
-        mv1= mult(mv2,rotateZ(this.rot));
+        mv1= mult(mv2, rotateX(this.rotX));
+        mv1 = mult( mv1, translate(this.x-this.x, this.y-this.y+1, this.z-this.z));
+        
         gl.uniformMatrix4fv(matrixLoc, false, flatten(mv1));
         gl.drawArrays( gl.TRIANGLES, 0,  NumVertices);
 
@@ -39,10 +45,13 @@ class Cube {
 
 
         var mv3 = mat4();
-        mv3= mult(mv3, rotateZ(this.rot))
         mv3 = mult( mvTemp, translate(this.x, this.y-1, this.z));
+        mv3= mult(mv2, rotateX(this.rotX));
+        mv3 = mult( mv3, translate(this.x-this.x, this.y-this.y-1, this.z-this.z));
         gl.uniformMatrix4fv(matrixLoc, false, flatten(mv3));
         gl.drawArrays( gl.TRIANGLES, 0,  NumVertices);
+        
+      
 
     }
 
@@ -50,8 +59,7 @@ class Cube {
 
     move(){
         this.keyHandlers();
-        this.rotZ+=0.1;
-        if(this.y-2 > -9.98)
+        if(this.y > -9)
             this.y -= gravity;
         else this.isDown = true;
     }
@@ -71,5 +79,18 @@ class Cube {
             else if(eatKey(38) && this.z > -2){
                 this.z-=1;
             }
+            else if(eatKey(65) && this.z > -2){
+                this.rotX+=90;
+            }
+            else if(eatKey(90) && this.z > -2){
+                this.rotX-=90;
+            }
+            else if(eatKey(83) && this.z > -2){
+                this.rotY+=90;
+            }
+            else if(eatKey(88) && this.z > -2){
+                this.rotY-=90;
+            }
+           
     }
 }
